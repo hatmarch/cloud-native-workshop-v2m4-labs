da68b107522fbdd811c5bbf61808a09cc5734220
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-01-27 01:40:53 +0000

    Update lab-guide as part of lab update script
    Changes where the workshopper deployment looks for .adoc content in git

--- workshop-supplement/scripts/update-lab.sh
+++ workshop-supplement/scripts/update-lab.sh
@@ -6,6 +6,10 @@ declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 declare -r USERS=( $(oc get project | grep -- -che | sed "s/-che.*$//g") )
 declare -r PASSWORD="r3dh4t1!"
 
+# Where to find the .adoc files for the workshopper pod in the cluster
+declare CONTENT_URL_PREFIX="https://raw.githubusercontent.com/hatmarch/cloud-native-workshop-v2m4-guides/ocp-4.5-sa"
+declare WORKSHOPS_URLS="_cloud-native-workshop-module4.yml"
+
 for USER in ${USERS[@]}; do
     PROJECT="${USER}-cloudnativeapps"
     echo "Importing imagestreams into ${PROJECT}"
@@ -17,3 +21,9 @@ for USER in ${USERS[@]}; do
     echo "Updating workspace for ${USER}"
     $SCRIPT_DIR/update-crw.sh ${USER} ${PASSWORD}
 done
+
+echo "Updating lab-guide content"
+oc set env -e CONTENT_URL_PREFIX="${CONTENT_URL_PREFIX}" -e WORKSHOPS_URLS="${CONTENT_URL_PREFIX}/${WORKSHOPS_URLS}" dc/guides-m4 -c guides-m4 -n labs-infra
+oc rollout status dc/guides-m4 -n labs-infra
+
+ 
