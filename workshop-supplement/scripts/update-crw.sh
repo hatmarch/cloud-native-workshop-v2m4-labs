17e3ec855a642355cb4df58d59174eeb2f18285a
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-01-28 04:27:26 +0000

    Fix update-crw to deal with stopped or missing workspaces
    This previously would fail on a missing or stopped workspace.  This now only fails if an existing workspace can't be deleted or created

--- workshop-supplement/scripts/update-crw.sh
+++ workshop-supplement/scripts/update-crw.sh
@@ -7,8 +7,19 @@ declare PASSWORD=${2}
 echo "In $0 for ${USER}"
 TOKEN=$(curl -X POST -s -d "username=${USER}&password=${PASSWORD}&grant_type=password&client_id=admin-cli" \
     https://$(oc get route keycloak -n labs-infra -o jsonpath='{.spec.host}')/auth/realms/codeready/protocol/openid-connect/token | jq -r '.access_token')
-WORKSPACE_ID=$(crwctl workspace:list -n labs-infra --access-token=${TOKEN} | grep ${USER}-workspace | awk '{ print $1 }')
-crwctl workspace:stop -n labs-infra --access-token=${TOKEN} ${WORKSPACE_ID}
-crwctl workspace:delete -n labs-infra --access-token=${TOKEN} ${WORKSPACE_ID}
+
+# Stop and delete any existing workspace
+WORKSPACE_ID=$(crwctl workspace:list -n labs-infra --access-token=${TOKEN} 2>/dev/null | grep ${USER}-workspace | awk '{ print $1 }')
+if [[ -z ${WORKSPACE_ID} ]]; then
+    echo "No existing workspace for ${USER}"
+else
+    # this will sometimes fail since workspaces may be stopped already
+    echo "Attempting to stop workspace ${WORKSPACE_ID} for user ${USER}"
+    crwctl workspace:stop -n labs-infra --access-token=${TOKEN} ${WORKSPACE_ID} 2>/dev/null || true
+
+    # if we fail to delete an existing workspace, then this is an error that we need to pay attention to
+    crwctl workspace:delete -n labs-infra --access-token=${TOKEN} ${WORKSPACE_ID}
+fi
+
 sed "s/%USER%/${USER}/g" ${DEMO_HOME}/Devfile_Template.yaml > /tmp/Devfile.yaml
 crwctl workspace:create -f /tmp/Devfile.yaml --access-token=${TOKEN} -n labs-infra --start
