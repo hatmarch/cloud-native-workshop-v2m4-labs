c059ba05427f66099731ee8085fa83ed91d44088
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-01-28 00:36:04 +0000

    Update the lab guides before each individual user

--- workshop-supplement/scripts/update-lab.sh
+++ workshop-supplement/scripts/update-lab.sh
@@ -3,12 +3,17 @@ set -Eeuo pipefail
 
 declare -r SCRIPT_DIR=$(cd -P $(dirname $0) && pwd)
 
-declare -r USERS=( $(oc get project | grep -- -che | sed "s/-che.*$//g") )
+#declare -r USERS=( $(oc get project | grep -- -che | sed "s/-che.*$//g") )
+declare -r USERS=( user2 )
 declare -r PASSWORD="r3dh4t1!"
 
 # Where to find the .adoc files for the workshopper pod in the cluster
 declare CONTENT_URL_PREFIX="https://raw.githubusercontent.com/hatmarch/cloud-native-workshop-v2m4-guides/ocp-4.5-sa"
 declare WORKSHOPS_URLS="_cloud-native-workshop-module4.yml"
+
+echo "Updating lab-guide content"
+oc set env -e CONTENT_URL_PREFIX="${CONTENT_URL_PREFIX}" -e WORKSHOPS_URLS="${CONTENT_URL_PREFIX}/${WORKSHOPS_URLS}" dc/guides-m4 -c guides-m4 -n labs-infra
+oc rollout status dc/guides-m4 -n labs-infra
 
 for USER in ${USERS[@]}; do
     PROJECT="${USER}-cloudnativeapps"
@@ -22,8 +27,6 @@ for USER in ${USERS[@]}; do
     $SCRIPT_DIR/update-crw.sh ${USER} ${PASSWORD}
 done
 
-echo "Updating lab-guide content"
-oc set env -e CONTENT_URL_PREFIX="${CONTENT_URL_PREFIX}" -e WORKSHOPS_URLS="${CONTENT_URL_PREFIX}/${WORKSHOPS_URLS}" dc/guides-m4 -c guides-m4 -n labs-infra
-oc rollout status dc/guides-m4 -n labs-infra
+
 
  
